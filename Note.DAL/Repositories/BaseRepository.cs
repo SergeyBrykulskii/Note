2b16f19646def5b60a05425fc03a44cb130ed9d4
Sergey Brykulskii <serxio6758sk@gmail.com>
2024-04-14 23:38:32 +0300

    added repositories

---
+++ Note.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Note.Domain.Interfaces.Repositories;
+
+namespace Note.DAL.Repositories;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public BaseRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<TEntity> CreateAsync(TEntity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        _dbContext.Add(entity);
+        _dbContext.SaveChanges();
+
+        return Task.FromResult(entity);
+    }
+
+    public Task<TEntity> RemoveAsync(TEntity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        _dbContext.Remove(entity);
+        _dbContext.SaveChanges();
+
+        return Task.FromResult(entity);
+    }
+
+    public IQueryable<TEntity> GetAll()
+    {
+        return _dbContext.Set<TEntity>();
+    }
+
+    public Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        _dbContext.Update(entity);
+        _dbContext.SaveChanges();
+
+        return Task.FromResult(entity);
+    }
+}
